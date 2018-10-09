a8f83ffbedcbb472b4fce77eb65763f8a8ac442c
kalleh <wallbomk@amazon.com>
2018-10-09 23:38:34 +0100

    fixed gitignore and template deployment

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
-aws s3 sync app/ s3://storage-kalleh/cfn/app
-aws cloudformation package --template-file template.yml --s3-bucket storage-kalleh --s3-prefix 'cfn/infrastructure' --output-template-file template-deploy.yml
-aws cloudformation deploy --template-file template-deploy.yml --stack-name gureume --capabilities CAPABILITY_NAMED_IAM
+S3_BUCKET="storage-kalleh"
+STACK_NAME="gureume-platform"
+
+aws s3 sync app/ s3://$S3_BUCKET/cfn/app
+aws cloudformation package --template-file template.yaml --s3-bucket $S3_BUCKET --s3-prefix 'cfn/infrastructure' --output-template-file template-deploy.yaml
+aws cloudformation deploy --template-file template-deploy.yaml --stack-name $STACK_NAME --capabilities CAPABILITY_NAMED_IAM
