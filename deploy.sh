349ee1c89d2f97cbd8c41ea60332ada5264895ca
kalleh <wallbomk@amazon.com>
2018-10-09 15:30:52 +0200

    streamlined infratemplates and removed ec2 instances by default

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 aws s3 sync app/ s3://storage-kalleh/cfn/app
-aws cloudformation package --template-file master.yaml --s3-bucket storage-kalleh --s3-prefix 'cfn/infrastructure' --output-template-file master-deploy.yaml
-aws cloudformation deploy --template-file master-deploy.yaml --stack-name gureume --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation package --template-file master.yml --s3-bucket storage-kalleh --s3-prefix 'cfn/infrastructure' --output-template-file master-deploy.yml
+aws cloudformation deploy --template-file master-deploy.yml --stack-name gureume --capabilities CAPABILITY_NAMED_IAM
