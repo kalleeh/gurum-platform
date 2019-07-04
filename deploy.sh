5cc2ec40c9d8834491faa0c21884843fe7a35999
kalleh <wallbomk@amazon.com>
2019-07-04 13:48:28 +0200

    moved product templates, renamed platform cfn dir

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 S3_BUCKET="storage-kalleh"
 STACK_NAME="gureume-platform"
 
-aws s3 sync cfn/ s3://$S3_BUCKET/cfn/
-aws cloudformation package --template-file template.yaml --s3-bucket $S3_BUCKET --s3-prefix 'platform-cfn' --output-template-file template-deploy.yaml
+aws cloudformation package --template-file template.yaml --s3-bucket $S3_BUCKET --s3-prefix 'gureume-platform' --output-template-file template-deploy.yaml
 aws cloudformation deploy --template-file template-deploy.yaml --stack-name $STACK_NAME --capabilities CAPABILITY_NAMED_IAM
