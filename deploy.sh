d3c75f77494a658e3f46aa111d1d96daf8d6b2f1
Karl Wallbom <karl.wallbom@gmail.com>
2022-07-28 10:22:37 +0200

    cleaning

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,18 @@ S3_BUCKET="gurum-deployment-artifacts-$ACCOUNT_ID-$REGION"
 if aws s3 ls "s3://$S3_BUCKET" 2>&1 | grep -q 'NoSuchBucket'; then
     echo "Creating initial artifacts bucket"
     aws s3 mb "s3://$S3_BUCKET"
+    aws s3api put-bucket-encryption --bucket $S3_BUCKET --server-side-encryption-configuration '{
+        "Rules": [
+            {
+                "ApplyServerSideEncryptionByDefault": {
+                    "SSEAlgorithm": "AES256"
+                }
+            }
+        ]
+    }'
+    aws s3api put-public-access-block \
+    --bucket $S3_BUCKET \
+    --public-access-block-configuration "BlockPublicAcls=true,IgnorePublicAcls=true,BlockPublicPolicy=true,RestrictPublicBuckets=true"
 fi
 
 # Only configure public hosted zone if it's not already configured
